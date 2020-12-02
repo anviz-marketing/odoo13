ed6dba055fbd1deadb1c438715d458c438e7fc89
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-12-02 14:18:09 +0800

    update

--- payment_elavon_scs/static/src/js/main.js
+++ payment_elavon_scs/static/src/js/main.js
@@ -9,7 +9,7 @@ odoo.define('payment_elavon.payment_elavon',function(require){
         })
 
     $('#o_payment_form_pay').click(function(){
-
+                                console.log($('.sale_order_id').val(),$('.order_id').val())
     ajax.jsonRpc("/payment/elavon_get_sale_order_detail", 'call', {"sale_order_id":$('.sale_order_id').val(),"sale_order_id":$('.order_id').val(),'inv_id':$('.inv_id').val()},{
         }).then(function(value){
 

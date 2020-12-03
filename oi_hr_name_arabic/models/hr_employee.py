fdd16df331c4b8816bd42e1e5269b51a39bedc9c
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-12-03 13:05:27 +0800

    update

--- oi_hr_name_arabic/models/hr_employee.py
+++ oi_hr_name_arabic/models/hr_employee.py
@@ -8,4 +8,5 @@ from odoo import models, fields, api
 class Employee(models.Model):
     _inherit = 'hr.employee'
     
-    name = fields.Char(translate = True)
+    #name = fields.Char(translate = True)
+    name = fields.Char(string="Employee Name", related='resource_id.name', store=True, readonly=False, tracking=True, translate = True)

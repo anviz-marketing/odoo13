2044552027e21b907c0316b62fbe54210f393bdf
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-12-18 01:08:58 +0800

    update

--- payment_elavon_scs/controllers/main.py
+++ payment_elavon_scs/controllers/main.py
@@ -44,8 +44,13 @@ class ElavonController(http.Controller):
                 methods=['GET', 'POST'], auth="public", csrf=False)
     def elavon_get_sale_order_detail(self, **post):
         try:
+            order_id = post.get('order_id')
             sale_order_id = post.get('sale_order_id')
-            sale_order = request.env['sale.order'].sudo().search([('id', '=', sale_order_id)])
+            if order_id:
+                domain = [('id', '=', order_id)]
+            else:
+                domain = [('id', '=', sale_order_id)]
+            sale_order = request.env['sale.order'].sudo().search(domain)
             vales = {
                 'id': sale_order.id,
                 'amount': sale_order.amount_total,

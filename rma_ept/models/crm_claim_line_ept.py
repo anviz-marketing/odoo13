513e643bff4859a6bc8e8ee896b8345ef6498f7b
Anviz <54052406+anviz-github@users.noreply.github.com>
2020-12-31 17:59:55 +0800

    update

--- rma_ept/models/crm_claim_line_ept.py
+++ rma_ept/models/crm_claim_line_ept.py
@@ -103,7 +103,7 @@ class CRMClaimLine(models.Model):
     # serial_lot_ids = fields.One2many("stock.production.lot", "claim_line_id", string="Lot/Serial "
     #                                                                                  "Number", )
     serial_lot_ids = fields.Many2many("stock.production.lot", string="Lot/Serial Number")
-    fail_description = fields.Char("Fail Description", require=True)
+    fail_description = fields.Char("fail description", string="Fail Description", require=True)
     warranty = fields.Selection([('in warranty', 'In Warranty'), ('out warranty', 'Out of Warranty'),
              ('Artificial', 'Artificial'), ('DOA', 'DOA')],
             "Warranty Type", require=True,copy=False)

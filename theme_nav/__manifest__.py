58eac332a987e3437f7dadfb0a8434d519600085
marketing@anviz.com <marketing@anviz.com>
2021-03-03 11:35:12 +0800

    update

---
+++ theme_nav/__manifest__.py
@@ -0,0 +1,13 @@
+{
+  'name':'kevin theme',
+  'description': 'anivz website nav',
+  'version':'1.0',
+  'author':'kevin.chen',
+ 
+  'data': [
+    'views/layout.xml',
+    'views/assets.xml'
+  ],
+  'category': 'Theme/Nav',
+  'depends': ['website','website_theme_install'],
+}
